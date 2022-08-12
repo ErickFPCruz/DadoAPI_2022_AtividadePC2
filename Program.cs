265a4f2017600b1e97ca257e9bf22ca10b357b73
ErickFPCruz <erick.cruz97@hotmail.com>
2022-08-11 21:33:45 -0300

    Aprendendo a programar backend com API's

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/dado/d{numeroDeFaces}", ([FromRoute] int numeroDeFaces) =>
+
+{   
+    if (numeroDeFaces <= 0)
+    {
+        return Results.BadRequest (new {mensagem = "Digite um número de faces maior que zero!!!!"});
+    }
+
+    int face = RandomNumberGenerator.GetInt32(1, numeroDeFaces + 1);
+    return Results.Ok (new {dado = $"d{numeroDeFaces}", rolagem = face});
+});
+
+app.Run();
